c3b816f18310b2710ebbd8058f02067ca83b13f5
Rahul Radhakrishnan <67397624+rahul1722@users.noreply.github.com>
2021-01-12 17:11:10 -0500

    Added image preprocessing steps
    Added Dilate and erode to img_preprocessing function

--- model.py
+++ model.py
@@ -7,8 +7,34 @@ from sklearn.utils import shuffle
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
+import random
 """
+def download(url, file):
+    if not os.path.isfile(file):
+        print("Download file... " + file + " ...")
+        urlretrieve(url,file)
+        print("File downloaded")
 
+download('https://drive.google.com/drive/folders/1KkwnvcR1rZpd0X0BQussLkHE6IRrXwDA?usp=sharing','data') #s3 path of the dataset provided by udacity
+
+print("All the files are downloaded")
+
+
+def uncompress_features_labels(dir,name):
+    if(os.path.isdir(name)):
+        print('Data extracted')
+    else:
+        with ZipFile(dir) as zipf:
+            zipf.extractall('data')
+uncompress_features_labels('data.zip','data')
+
+
+def data_Files(mypath):
+    onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+    print(onlyfiles)
+
+print('All files downloaded and extracted')
+"""
 
 def random_distort(img, angle):
     
@@ -37,7 +63,7 @@ def random_distort(img, angle):
     M = cv2.getPerspectiveTransform(pts1,pts2)
     new_img = cv2.warpPerspective(new_img,M,(w,h), borderMode=cv2.BORDER_REPLICATE)
     return (new_img.astype(np.uint8), angle)
-"""
+
 
 def img_preprocess(image):
     #new_image = image.astype(float)
@@ -48,37 +74,57 @@ def img_preprocess(image):
     new_image = cv2.cvtColor(new_image, cv2.COLOR_YUV2RGB)
     return new_image
 
-def generate_training_data(image_paths, angles, batch_size, validation_flag=False):
+def generate_training_data(image_paths, angles, batch_size): #validation_flag=False):
     '''
     method for the model training data generator to load, process, and distort images, then yield them to the
     model. if 'validation_flag' is true the image is not distorted. also flips images with turning angle magnitudes of greater than 0.33, as to give more weight to them and mitigate bias toward low and zero turning angles
     '''
     image_paths, angles = shuffle(image_paths, angles)
-    X,y = ([],[])
+    #X = np.zeros((batch_size,160,320,3))
+    #y = np.zeros((batch_size,1))
+    #X = np.array()
+    #y = np.array()
     while True:       
-        for i in range(len(angles)):
-            img = image_paths[i]
+        for i in range(batch_size):
+            #index = random.randint(1,len(image_paths))
+            img = img_preprocess(image_paths[i])
             angle = angles[i]
-            img = img_preprocess(img)
-            if not validation_flag:
-                img, angle = random_distort(img, angle)
+            
+            if abs(angle) > 0.2:
+                #X.append(img)
+                #y.append(angle)
+                img = cv2.flip(img,1)
+                angle = angle * -1
+            #X.append(img)
+            #y.append(angle)
+            yield img,angle
+        
+            #img = img_preprocess(img)
+            #angle = angles[index]
+    
+            #if not validation_flag:
+                #img, angle = random_distort(img, angle)
+            
+            #X.append(img)
+            #y.append(angle)
+            #if len(X) == batch_size:
+                #break
+       
+        X, y = ([],[])
+        #image_paths, angles = shuffle(image_paths, angles)
+        # flip horizontally and invert steer angle, if magnitude is > 0.33
+"""   
+        if abs(angle) > 0.33:
+            img = cv2.flip(img, 1)
+            angle *= -1
             X.append(img)
             y.append(angle)
             if len(X) == batch_size:
-                yield (np.array(X), np.array(y))
-                X, y = ([],[])
-                image_paths, angles = shuffle(image_paths, angles)
-            # flip horizontally and invert steer angle, if magnitude is > 0.33
-            if abs(angle) > 0.33:
-                img = cv2.flip(img, 1)
-                angle *= -1
-                X.append(img)
-                y.append(angle)
-                if len(X) == batch_size:
-                    yield (np.array(X), np.array(y))
-                    X, y = ([],[])
-                    image_paths, angles = shuffle(image_paths, angles)
-    
+                break
+        yield (np.array(X), np.array(y))
+                #X, y = ([],[])
+                #image_paths, angles = shuffle(image_paths, angles)
+"""
     
 def generate_training_data_for_visualization(images, angles, batch_size=20, validation_flag=False):
     '''
@@ -102,9 +148,11 @@ def generate_training_data_for_visualization(images, angles, batch_size=20, vali
 lines = []
 
 
-with open('data/driving_log.csv') as csv_file:
+with open('/opt/carnd_p3/data/driving_log.csv') as csv_file:
+    
     reader = csv.reader(csv_file)
-    #next(csv_file)
+    #csv.field_size_limit(15000)
+    next(csv_file)
     for line in reader:
         lines.append(line)
         
@@ -115,9 +163,9 @@ correction = 0.2
 for column in lines:
     for i in range(3):
         source_path = column[i]
-        file_name = source_path.split('\\')[-1]
-        current_path = ('data/IMG/' + file_name)
-        image = cv2.cvtColor(cv2.imread(current_path), cv2.COLOR_BGR2RGB)
+        file_name = source_path.split('/')[-1]
+        current_path = ('/opt/carnd_p3/data/IMG/'+file_name)
+        image = ndimage.imread(current_path)
         if i==0:
             measurement = float(column[3])
         elif i==1:
@@ -181,33 +229,38 @@ model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((60,20), (0,0))))
 model.add(Convolution2D(24,(5,5), strides=(2,2), activation='relu'))
+model.add(Dropout(0.3))
 model.add(Convolution2D(36,(5,5), strides=(2,2), activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Convolution2D(48,(5,5), strides=(2,2), activation='relu'))
+model.add(Dropout(0.3))
 model.add(Convolution2D(64,(3,3), activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 #model.add(Convolution2D(76,3,3, activation='relu'))
 #model.add(Convolution2D(88,3,3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(50))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(10))
 #model.add(Dropout(0.5))
 model.add(Dense(1))
 
-bs = 70
+#bs = 30
 
-train_data = generate_training_data(Image_train, angle_train, batch_size=bs, validation_flag=False)
-valid_data = generate_training_data(Image_train, angle_train, batch_size=bs, validation_flag=True)
-test_data = generate_training_data(Image_test, angle_test, batch_size=bs, validation_flag=True)
+train_data = generate_training_data(Image_train, angle_train, batch_size = 70)#, validation_flag=False)
+valid_data = generate_training_data(Image_train, angle_train, batch_size = 20)#, validation_flag=True)
+test_data = generate_training_data(Image_test, angle_test, batch_size = 20)#, validation_flag=True)
 
 #print(train_data.type)
 #print(valid_data.shape)
 #print(test_data.shape)
 
 model.compile(loss='mse',optimizer='adam')
-model.fit_generator(train_data, epochs=7, validation_data=valid_data, steps_per_epoch=50, validation_steps=50)
+model.fit_generator(train_data, epochs=2, validation_data=valid_data, steps_per_epoch=30, validation_steps=20)
 
 model.save('model.h5')
+
+ 
+    
